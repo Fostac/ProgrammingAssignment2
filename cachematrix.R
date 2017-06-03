826b13dba91df830bf4ae31cdf156244e4b848fb
Fostac <marius@bgst.de>
2017-06-03 14:21:09 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these pair of functions let you convert an invertible matrix to a special 
+## matrix that is cached after being calculated once. Afterwards the result
+## will be taken from the cache instead of beeing calculated a second time.
+## Usage:
+## myMat<- ##define invertible matrix
+## myCachedMat<-makeCacheMatrix(myMat)
+## cacheSolve(myCachedMat)
 
-## Write a short comment describing this function
+## function makeCacheMAtrix takes an invertible matrix and converts it to a special
+## matrix for the cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
+  
 }
 
 
-## Write a short comment describing this function
+## function cacheSolve gives the inverse of a matrix as an output. if the matrix
+## has been calculated before the result is taken from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  
+  ## Return a matrix that is the inverse of 'x'
 }
